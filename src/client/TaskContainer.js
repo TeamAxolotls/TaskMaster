15981cca22454dc19fb5f4c70a7696b2fefc3b82
Steven Yuan <stevenyuan@Stevens-MacBook-Pro.local>
2022-06-25 11:31:47 -1000

    task updated version

---
+++ src/client/TaskContainer.js
@@ -0,0 +1,28 @@
+import react, { useEffect } from 'react';
+import MainTask from './MainTask'
+
+const TaskContainer = () => { 
+
+    //set a task state;
+    const [task, setTask] = useState('enter a task');
+    //set a sub-task state
+    const [subTask, setSubTask] = useState('');
+
+    //fetch for data from database, then use it to set state
+    useEffect(() => {
+
+        fetch ('http://localhost:3000') //get all data from database
+          .then (res =>{
+            console.log(res)
+          })
+    });
+
+    return (
+        <div>
+            <MainTask/>
+        </div>
+    )
+
+};
+
+export default TaskContainer;
