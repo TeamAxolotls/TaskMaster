ed1d5649b08732f360261caadf2bcb084218cc16
Gabriel <gkime91@gmail.com>
2022-06-25 14:10:57 -0700

    got get request to html file working. added error handling for 404 routes. still working on getting css to route through backend.

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,27 @@
 const express = require ('express');
 const path = require ('path');
+const webpack = require('webpack')
+const fs = require ('fs');
 
-const app = express();
+// initialize port to localhost 3000
 const port = 3000;
+
+//initialize server
+const app = express();
+
+// send get request to index.html
+
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname, '../../dist/index.html'));
+})
+
+app.use('../src/client/styles.css', (req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname, '../client/styles.css'));
+})
+
+app.use((req, res) => {
+  res.sendStatus(404);
+})
+
+
 app.listen(3000, ()=>{console.log(`listening on port 3000`)});
