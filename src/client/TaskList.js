e3ba0a70dde35c480e4f94a26442948ffd1092d8
Ben Engelberg <benjaminsengelberg@gmail.com>
2022-06-27 15:45:35 -0700

    subtask frontend done. just need to connect to backend now

--- src/client/TaskList.js
+++ src/client/TaskList.js
@@ -1,12 +1,16 @@
 import React from "react";
 import uncompleted from "../img/circle.png";
 import completed from "../img/circlegreencheck.png"
+import completedSub from "../img/circleyellowcheck.png"
+import add from "../img/add-symbol.png"
 import { useState } from "react";
-const checks = {uncompleted, completed}; // These are the unchecked and checked circles
+const checks = {uncompleted, completed}; 
+const subChecks = {uncompleted, completedSub};// These are the unchecked and checked circles
 
 const task = [{taskItem: "Sell my laptop", highlight: false, identifier: 1, completed: false},
 {taskItem: "Pack for vacation to SF", highlight: false, identifier: 2, completed: false},
-{taskItem: "Study for graduation assessment", highlight: false, identifier: 3, completed: false},
+{taskItem: "Study for graduation assessment", highlight: false, identifier: 3, completed: false},{taskItem: "Sell my laptop", highlight: false, identifier: 1, completed: false},
+{taskItem: "Pack for vacation to SF", highlight: false, identifier: 2, completed: false},
 ];
 const subtask = [{pointer: 1, tasks:['Look up current value of computer', 'Create ad and post to craigslist']}, 
     {pointer: 2, tasks: ["Do laundry and choose clothes", "Go shopping for travel items", "Buy plane ticket"]},
@@ -50,15 +54,56 @@ const TaskList = (props)=> {
             return subtask.pointer === obj.identifier
         });
     subtaskArr = subtaskArr[0].tasks;
-        // console.log(subtaskArr);
     const subtaskDivs = [];
 
+
+    // Subtask image change function, and to create entire subtask Div
     for (let i = 0; i < subtaskArr.length; i++) {
-        subtaskDivs.push(<li>{subtaskArr[i]}</li>)
+        const [subCheck, setSubCheck] = useState(subChecks.uncompleted);
+        function completeSub() {
+            if (subCheck === subChecks.completedSub) {
+                setSubCheck(subChecks.uncompleted)
+            } else {
+                setSubCheck(subChecks.completedSub)
+            }
+        };
+        subtaskDivs.push(
+        <div className="subtask-container">
+            <li className="subtask-item">{subtaskArr[i]}</li>
+            <button onClick={completeSub} id="subtask-completed-button"><img src={subCheck} height="30px"></img></button> 
+        </div>
+        )
     }
 
-    console.log(subtaskDivs);
+    // Function and state for displaying subtask input field and button
+    const InputSubTask = () => {
+        const [showInput, setShowInput] = useState(false);
+        const clicked = () => {
         
+        if (showInput === false) setShowInput(true);
+        if (showInput === true) setShowInput(false);
+        // setShowInput(true);
+        }
+
+        return (
+            <div id="add-subtask-container">
+                              { showInput ? <SubTaskInputField /> : null }
+              <button onClick={clicked} id="add-subtask-button"><img src={add} height="25px"/></button>
+
+            </div>
+        )
+    }
+    
+    const SubTaskInputField = () => (
+        <div id="subtask-input-div">
+            <input id="subtask-input-field"></input><button id="submit-subtask-button">
+                {/* <img src={add} height="19px"></img> */}
+            </button>
+        </div>
+    )
+    ////////////////////////////////////////////////////////////////////
+
+
         return (
             <div className="tasks-container">   
                 <div className="main-task">{obj.taskItem}<button id="check-circle" 
@@ -67,6 +112,7 @@ const TaskList = (props)=> {
                 </div>
                 <div className="sub-tasks">
                      { subtaskDivs }
+                     <InputSubTask />
                 </div>
             </div>
 
